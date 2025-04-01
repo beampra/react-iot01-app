95c0857e4e6f4cd12e300c33dd712576910249ea
beampra <s6419410001@sau.ac.th>
2025-04-01 10:58:42 +0700

    v2: แก้หน้า HomeMenu

--- src/views/HomeMenu.jsx
+++ src/views/HomeMenu.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Box, Typography, Button, Avatar } from '@mui/material'
 
 export default function HomeMenu() {
   return (
-    <div>Nontakorn Pravatvithisuk</div>
+    <>
+      <Box sx={{ height: '100vh', display: 'flex' }}>
+        <Box sx={{ width: '70%', boxShadow: 2, mx: 'auto', my: 'auto', p: 3 }}>
+          <Avatar src={'https://cdn-icons-png.flaticon.com/512/10355/10355707.png'} sx={{ width: 150, height: 150, mx: 'auto' }}></Avatar>
+          <Typography variant='h3' sx={{ textAlign: 'center', mt: 2 }}>IoT Calculate by Nontakorn SAU</Typography>
+        </Box>
+      </Box>
+    </>
   )
 }
